c09bfc55de5420031c63885d1dddc25a9fde4421
Maxime Beugnet <maxime.beugnet@zenika.com>
2017-01-16 15:38:05 +0100

    first commit with maven

---
+++ src/main/java/com/github/elasticHelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.github.elasticHelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
